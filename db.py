771088121819715870c51ea6ba36677b04acbfff
IheCharles <cci29@drexel.edu>
2024-04-06 10:46:15 -0400

    start

---
+++ db.py
@@ -0,0 +1,34 @@
+import pymysql
+
+conn = pymysql.connect(
+    host='sql3.freesqldatabase.com', 
+    user='sql3697196',
+    password='DELhWFq2K5', 
+    db='sql3697196', 
+    port=3306,  
+    charset='utf8mb4',  
+    cursorclass=pymysql.cursors.DictCursor
+
+)
+cursor = conn.cursor()
+
+authors_query = """CREATE TABLE authors (
+id serial PRIMARY KEY,
+name text,
+email text,
+date_of_birth timestamp
+)"""
+
+books_query = """CREATE TABLE books (
+id serial PRIMARY KEY,
+author_id integer REFERENCES authors (id),
+isbn text,
+);"""
+
+sale_items_query = """CREATE TABLE sale_items (
+id serial PRIMARY KEY,
+book_id integer REFERENCES books (id),
+customer_name text,
+item_price money,
+quantity integer
+);"""
